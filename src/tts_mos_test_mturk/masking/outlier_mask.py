f4badc03564df94141d01ac2f676dff2a81e0664
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2023-02-20 18:05:13 +0100

    improved logging

--- src/tts_mos_test_mturk/masking/outlier_mask.py
+++ src/tts_mos_test_mturk/masking/outlier_mask.py
@@ -3,26 +3,40 @@ from typing import Set
 import numpy as np
 
 from tts_mos_test_mturk.evaluation_data import EvaluationData
+from tts_mos_test_mturk.logging import get_detail_logger
 from tts_mos_test_mturk.statistics.update_stats import print_stats_masks
 
 
-def mask_outliers(Z: np.ndarray, max_std_dev_diff: float) -> np.ndarray:
+def get_mu_norm(Z: np.ndarray) -> np.ndarray:
   assert len(Z.shape) == 2
   mu = np.nanmean(Z)
   s = np.nanstd(Z)
 
   mu_norm = abs(Z - mu) / s
-  outlying_ratings: np.ndarray = mu_norm > max_std_dev_diff
+  return mu_norm
 
+
+def mask_outliers(Z: np.ndarray, max_std_dev_diff: float) -> np.ndarray:
+  mu_norm = get_mu_norm(Z)
+  outlying_ratings = mu_norm > max_std_dev_diff
   return outlying_ratings
 
 
 def mask_outliers_alg(ratings: np.ndarray, max_std_dev_diff: float) -> np.ndarray:
+  dlogger = get_detail_logger()
   result = np.full_like(ratings, fill_value=False, dtype=bool)
   n_alg = ratings.shape[0]
   for alg_i in range(n_alg):
     Z = ratings[alg_i]
-    result[alg_i, :] = mask_outliers(Z, max_std_dev_diff)
+    mu_norm = get_mu_norm(Z)
+    masked = mu_norm > max_std_dev_diff
+    result[alg_i, :] = masked
+    dlogger.info(f"Algorithm {alg_i}:")
+    dlogger.info(f"- min: {np.nanmin(mu_norm)}")
+    dlogger.info(f"- mean: {np.nanmean(mu_norm)}")
+    dlogger.info(f"- median: {np.nanmedian(mu_norm)}")
+    dlogger.info(f"- max: {np.nanmax(mu_norm)}")
+    dlogger.info(f"- masked: {np.sum(masked)}")
   return result
 
 
@@ -30,8 +44,9 @@ def mask_outlying_ratings(data: EvaluationData, mask_names: Set[str], max_std_de
   masks = data.get_masks_from_names(mask_names)
   factory = data.get_mask_factory()
 
-  ratings = data.get_ratings()
   rmask = factory.merge_masks_into_rmask(masks)
+
+  ratings = data.get_ratings()
   rmask.apply_by_nan(ratings)
 
   outlier_np_mask = mask_outliers_alg(ratings, max_std_dev_diff)
