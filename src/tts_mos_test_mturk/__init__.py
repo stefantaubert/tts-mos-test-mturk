75b2a559c84c23654bee093e5d29d7546d8a1407
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2023-02-23 11:12:06 +0100

    sorting

--- src/tts_mos_test_mturk/__init__.py
+++ src/tts_mos_test_mturk/__init__.py
@@ -1,3 +1,5 @@
+from tts_mos_test_mturk.calculation.correlations import \
+  get_algorithm_mos_correlations as algorithm_correlation
+from tts_mos_test_mturk.calculation.correlations import \
+  get_sentence_mos_correlations_3dim as sentence_correlation
 from tts_mos_test_mturk.calculation.mos_variance import compute_ci95, compute_mos
-from tts_mos_test_mturk.calculation.correlations import get_sentence_mos_correlations_3dim as sentence_correlation
-from tts_mos_test_mturk.calculation.correlations import get_algorithm_mos_correlations as algorithm_correlation
