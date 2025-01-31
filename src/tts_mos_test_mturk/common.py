dad37aca2ae0b0f5eb0874124a6cf10c53167bb1
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-01-31 14:53:41 +0100

    add get_duplicate_votes_mad

--- src/tts_mos_test_mturk/common.py
+++ src/tts_mos_test_mturk/common.py
@@ -1,9 +1,49 @@
-from typing import List, Set
+from collections import defaultdict
+from typing import Dict, List, Set
 
 import numpy as np
 
 from tts_mos_test_mturk.evaluation_data import EvaluationData
-from tts_mos_test_mturk.typing import RatingName, Ratings, RatingValue
+from tts_mos_test_mturk.typing import RatingName, Ratings, RatingValue, WorkerName
+
+
+def get_duplicate_votes_mad(data: EvaluationData, rating_names: Set[str]):
+  from scipy.stats import median_abs_deviation
+
+  ratings: List[List[List[List[RatingValue]]]] = [
+    [
+      [
+        []
+        for _ in range(data.n_files)
+      ]
+      for _ in range(data.n_workers)
+    ]
+    for _ in range(data.n_algorithms)
+  ]
+
+  for worker, worker_data in data.worker_data.items():
+    worker_i = data.workers.get_loc(worker)
+    for assignment_data in worker_data.assignments.values():
+      for (alg_name, file_name), ass_ratings in assignment_data.ratings.items():
+        alg_i = data.algorithms.get_loc(alg_name)
+        file_i = data.files.get_loc(file_name)
+        rating = get_mean_rating(ass_ratings.votes, rating_names)
+        ratings[alg_i][worker_i][file_i].append(rating)
+
+  final_ratings = np.full(
+    (data.n_algorithms, data.n_workers, data.n_files),
+    fill_value=np.nan,
+    dtype=np.float32
+  )
+
+  for alg_i in range(data.n_algorithms):
+    for worker_i in range(data.n_workers):
+      for file_i in range(data.n_files):
+        rs = ratings[alg_i][worker_i][file_i]
+        if len(rs) > 1:
+          final_ratings[alg_i, worker_i, file_i] = median_abs_deviation(rs)
+
+  return final_ratings
 
 
 def get_ratings(data: EvaluationData, rating_names: Set[str]) -> np.ndarray:
